6ee046a70d7143bdcda71f90f1dd35f7dfe6eeee
Sree Vaishnavi <v26502848@gmail.com>
2026-05-25 18:46:09 +0530

    debug database env

--- database.js
+++ database.js
@@ -1,5 +1,8 @@
 const { Pool } = require("pg");
 
+console.log("🔥 DATABASE MODULE LOADED");
+console.log("DATABASE_URL =", process.env.DATABASE_URL ? "EXISTS" : "MISSING");
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false }
